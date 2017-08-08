ac424a2d84fcb159c7baacedcacaaea9e310e614
Tewi <tewisp@gmail.com>
2017-08-07 23:05:13 -0400

    add AND perceptron

---
+++ PerceptronAND.py
@@ -0,0 +1,8 @@
+class PerceptronAND:
+    def __init__(self):
+        self.w1 = 1
+        self.w2 = 1
+        self.bias = 2
+
+    def output(self, in1, in2):
+        return 1 if (self.w1*in1 + self.w2*in2) >= self.bias  else 0
